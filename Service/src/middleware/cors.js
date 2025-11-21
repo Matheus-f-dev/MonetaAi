c9df4b84e640aceaf66966f735497f1ed6612bd5
Matheusfreire <matheus.f.a.dev@gmail.com>
2025-11-21 13:29:28 -0300

    cors

--- Service/src/middleware/cors.js
+++ Service/src/middleware/cors.js
@@ -1,17 +1,30 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'https://www.monetaai.site',
-    'https://monetaai.site',
-    'https://monetaai-frontend-2pemd90yq-matheus-projects-9993ec57.vercel.app'
-  ],
+  origin: function (origin, callback) {
+    // Permitir requisições sem origin (mobile apps, etc.)
+    if (!origin) return callback(null, true);
+    
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'https://www.monetaai.site',
+      'https://monetaai.site',
+      'https://monetaai-frontend-2pemd90yq-matheus-projects-9993ec57.vercel.app'
+    ];
+    
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log('CORS blocked origin:', origin);
+      callback(null, true); // Temporariamente permitir todos
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
+  preflightContinue: false
 };
 
 module.exports = cors(corsOptions);
