af5c71d48136989bcddd27d71780625bcbc62172
Matheusfreire <matheus.f.a.dev@gmail.com>
2025-11-21 12:48:34 -0300

    arrumando cors

--- Service/src/middleware/cors.js
+++ Service/src/middleware/cors.js
@@ -1,10 +1,17 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: true,
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://www.monetaai.site',
+    'https://monetaai.site',
+    'https://monetaai-frontend-2pemd90yq-matheus-projects-9993ec57.vercel.app'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  optionsSuccessStatus: 200
 };
 
 module.exports = cors(corsOptions);
